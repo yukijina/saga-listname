c39e77acd2b329a62fbff1de93b29b223275e675
yukijina <yukijina@gmail.com>
2020-11-08 11:45:06 -0800

    create user. setup store, connect reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import reducers from './reducers'
 import reportWebVitals from './reportWebVitals';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api'
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
